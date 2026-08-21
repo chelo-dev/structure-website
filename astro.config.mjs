85ca594bcc55fbf3c6db6f8bcabc8d6b23a6ee3a
dev-chelo <angelparedestorres.apt@gmail.com>
2026-08-21 17:41:44 -0600

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ export default defineConfig({
   integrations: [sitemap()],
   compressHTML: true,
   build: {
-    inlineStylesheets: 'auto',
+    inlineStylesheets: 'always',
   },
   vite: {
     build: {
